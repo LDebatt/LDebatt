30cc0c510e1c316422bb8d2c328e53ad636b375c
l.debattista <l.debattista@betagy.com>
2021-10-23 13:29:58 +0200

    Finished calculation of expression and checked for validity

--- validExpressionApp/src/app/app.component.ts
+++ validExpressionApp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import math from 'mathjs-expression-parser';
+import _clone from 'lodash/clone'
+import _escapeRegExp from 'lodash/escapeRegExp'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'validExpressionApp';
+  title = 'Valid Math Expressions';
+  state: { expression: string; result: string; validity: string; };
+
+  constructor() {
+    this.state = {expression: '', result: '', validity: ''};
+  }
+
+  getValidityColor = () => {
+    if (this.state.validity === 'Good') {
+      return 1;
+    } else if (this.state.validity === 'Not valid') {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  getResult = (mathString) => {
+    let result;
+    try {
+      result = math.eval(mathString);
+      if (this.state.validity !== 'Good') {
+        this.setState('validity', 'Good' );
+      }
+    }
+    catch (err){
+      console.log(err);
+      result = '';
+      if (this.state.validity !== 'Not valid') {
+        this.setState( 'validity', 'Not valid');
+      }
+    }
+    return result;
+  }
+
+  setState = (key: string, value: any) => {
+    switch (key) {
+      case 'validity':
+        this.state.validity = value;
+        break;
+      case 'expression':
+        this.state.expression = value;
+        break;
+      case 'result':
+        this.state.result = value;
+        break;
+      default:
+        break;
+    }
+  }
+
+  subFields = (expression) => {
+    let swappedExpression = _clone(expression);
+    const variables = swappedExpression.match(/\{[^\{\}]+\}/gi) || [];
+    const variablesArray = variables.map(myVariableWithBrackets => {
+      return myVariableWithBrackets.slice(1, -1);
+    });
+    variablesArray.map(myVariable => {
+      swappedExpression = swappedExpression.replace(
+        new RegExp( '{' + _escapeRegExp(myVariable) + '}', 'gi'), '1' );
+    });
+    return swappedExpression;
+  }
+
+  checkExpression = () => {
+    const subExpression = this.subFields(this.state.expression);
+    this.setState('result', this.getResult(subExpression));
+  }
+
+  handleChange = (e) => {
+    this.setState('expression', e.target.value);
+    this.checkExpression();
+  }
 }
